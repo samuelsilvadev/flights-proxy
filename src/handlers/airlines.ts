c7b890c0cae2997e78580b5d3e1f95a415bb2b59
Samuel Silva <samuelsilvawb@gmail.com>
2023-09-28 00:09:34 +0100

    style: update formatting

--- src/handlers/airlines.ts
+++ src/handlers/airlines.ts
@@ -5,13 +5,15 @@ const ENDPOINT = "/airlines";
 const API_VERSION = "1";
 
 function buildResponse(response: unknown) {
-    return JSON.stringify({
-        originalResponse: response, date: new Date().toISOString(), version: API_VERSION 
-    })
+	return JSON.stringify({
+		originalResponse: response,
+		date: new Date().toISOString(),
+		version: API_VERSION,
+	});
 }
 
 function buildDefaultHeaders() {
-    return { "Content-Type": "application/json" }
+	return { "Content-Type": "application/json" };
 }
 
 export async function getAirlinesHandler(context: Context) {
